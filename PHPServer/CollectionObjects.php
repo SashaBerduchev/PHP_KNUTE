c945fb81a65e67101fce7b66965dd3eff03d4fcf
Санёк Виничук <sashavinichuk@gmail.com>
2022-10-13 19:55:57 +0300

    CollectionObjects.php WorkObject.php

--- PHPServer/CollectionObjects.php
+++ PHPServer/CollectionObjects.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace PHP_KNUTE;
 class CollectionObjects
 {
     var $map;
--- PHPServer/WorkObject.php
+++ PHPServer/WorkObject.php
@@ -6,9 +6,12 @@ $typecount = $_POST["typecount"];
 $type = $_POST["type"];
 $price = $_POST["price"];
 setResult($Name, $Cech, $typecount, $type, $price);
-include_once CollectionObjects::class;
+
 function setResult($name, $cech, $typecount, $type, $price){
-    $objects = new CollectionObjects;
+    spl_autoload_register(function ($class_name) {
+        include $class_name . '.php';
+    });
+    $objects = new \PHP_KNUTE\CollectionObjects();
     $objects->addElem($name, $cech, $typecount, $type, $price);
 
 }
