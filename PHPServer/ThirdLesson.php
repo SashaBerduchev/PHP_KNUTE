1f98f6a8c19a6ab28b58eb7d19bd7ebaadb3cb89
Санёк Виничук <sashavinichuk@gmail.com>
2022-11-17 19:34:50 +0200

    ThirdLesson.php Lesson2.html index.html

--- PHPServer/ThirdLesson.php
+++ PHPServer/ThirdLesson.php
@@ -1,9 +1,24 @@
 <?php
 
-$index = document.getElementById('index');
-$street = document.getElementById('street');
-$city = document.getElementById('Cities');
-$house = document.getElementById('house');
-$flat = document.getElementById('flat');
-$selvalue = city.value;
-$res = document.getElementById('res');
+$index =  $_POST['index'];
+$street = $_POST['street'];
+$city = $_POST['Cities'];
+$house = $_POST['house'];
+$flat = $_POST['flat'];
+$selvalue = $city;
+if($index == '' || $street == '' || $city == '' || $house == '' || $flat == ''){
+    alert("Fields are not filled");
+    return;
+}else if(preg_match("/^\d+$/", $index)==false){
+    alert('Index not number');
+    return;
+}else if(preg_match("/^\d+$/", $flat)== false){
+    alert('Flat not number');
+    return;
+}else if(preg_match("/^\w/", $street)==false){
+    echo "Street must be a string";
+    return;
+}else{
+    $addres = $selvalue + ', ' + $street + ', ' + $house + ', ' + $flat + ', ' + $index;
+        echo $addres;
+    }
