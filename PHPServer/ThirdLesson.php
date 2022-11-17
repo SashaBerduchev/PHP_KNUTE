7d58baba33eee22e246c35c8dbdd3e167b88d69b
Санёк Виничук <sashavinichuk@gmail.com>
2022-11-17 19:09:52 +0200

    ThirdLesson.php

---
+++ PHPServer/ThirdLesson.php
@@ -0,0 +1,9 @@
+<?php
+
+$index = document.getElementById('index');
+$street = document.getElementById('street');
+$city = document.getElementById('Cities');
+$house = document.getElementById('house');
+$flat = document.getElementById('flat');
+$selvalue = city.value;
+$res = document.getElementById('res');
